356396c53888dab8965b5a308fb77d0163e2f0d8
Radhika Patel <radhika.patel@ajackus.com>
2022-08-08 15:04:52 +0530

    change ui as per design

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
-import { Box, Grid, Tabs, Tab } from '@mui/material';
+import {
+  Box, Grid, Tabs, Tab, Button, Typography,
+} from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
+import { updateProfile } from '@redux/authuser/authuser.actions';
 import { routes } from '@routes/routesConstants';
 import General from './General/General';
 import Profile from './Profile/Profile';
@@ -11,13 +14,24 @@ const useStyles = makeStyles((theme) => ({
   container: {
     padding: `0 ${theme.spacing(10)}`,
   },
+  upload: {
+    height: theme.spacing(20),
+  },
+  userAvatar: {
+    width: theme.spacing(20),
+    borderRadius: theme.spacing(1),
+    border: `1px solid ${theme.palette.primary.contrastText}`,
+  },
+  button: {
+    margin: `${theme.spacing(1)} ${theme.spacing(1)}`,
+  },
   tabs: {
     borderBottom: `1px solid ${theme.palette.primary.contrastText}`,
     padding: `0 ${theme.spacing(20)}`,
   },
 }));
 
-const Dashboard = ({ history }) => {
+const Dashboard = ({ history, dispatch, user }) => {
   const classes = useStyles();
   const subNav = [
     { label: 'General', value: 'general' },
@@ -26,6 +40,8 @@ const Dashboard = ({ history }) => {
   const viewPath = (subNav.find((item) => location.pathname.endsWith(item.value))
   || subNav[1]).value;
   const [view, setView] = useState(viewPath);
+  const hiddenFileInput = useRef(null);
+  const [file, setFile] = useState(null);
 
   // this will be triggered whenever the content switcher is clicked to change the view
   useEffect(() => {
@@ -37,23 +53,83 @@ const Dashboard = ({ history }) => {
     setView(view);
   };
 
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleCancel = (event) => {
+    hiddenFileInput.current.value = null;
+    setFile(null);
+  };
+
+  const handleUpdate = (event) => {
+    const formData = new FormData();
+    if (file) {
+      formData.append('file', file[0], file[0].name);
+    }
+    formData.append('organization_name', user.organization.name);
+    dispatch(updateProfile(user.id, formData));
+    hiddenFileInput.current.value = null;
+    setFile(null);
+  };
+
   return (
-  <Box mt={1} mb={3} style={{ padding: '6em 3em' }}>
-    <Grid mb={3} container className={classes.container}>
-      <Grid item xs={12}>
-        <Tabs value={view} onChange={viewTabClicked} className={classes.tabs}>
-          {subNav.map((itemProps, index) => <Tab {...itemProps} key={`tab${index}:${itemProps.value}`} />)}
-        </Tabs>
+    <Box mt={1} mb={3} style={{ padding: '6em 3em' }}>
+      <Grid mb={3} container className={classes.container}>
+        <Grid item xs={12} textAlign="center" mb={1.5}>
+          {user && !user.avatar_url && !file && (
+            <Button variant="outlined" component="label" className={`${classes.upload} ${classes.userAvatar}`} onClick={handleClick}>
+              {(file && file[0] && file[0].name) || 'Upload'}
+            </Button>
+          )}
+          {user && user.avatar_url && !file && (
+            <Button onClick={handleClick}>
+              <img src={user.avatar_url} alt={user.first_name} className={classes.userAvatar} />
+            </Button>
+          )}
+          {file && file[0] && (
+            <>
+              <Button onClick={handleClick}>
+                <img
+                  src={URL.createObjectURL(file[0])}
+                  alt={file[0].name}
+                  className={classes.userAvatar}
+                />
+              </Button>
+              <div>
+                <Button className={classes.button} type="button" variant="contained" onClick={handleUpdate}>
+                  Update
+                </Button>
+                <Button className={classes.button} type="button" variant="contained" onClick={handleCancel}>
+                  Cancel
+                </Button>
+              </div>
+            </>
+          )}
+          <input ref={hiddenFileInput} hidden accept="image/*" type="file" onChange={(e) => setFile(e.target.files)} />
+        </Grid>
+        <Grid item xs={12} textAlign="center" mb={3}>
+          <Typography variant="h6" component="h6">
+            {`${user.first_name} ${user.last_name}`}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Tabs value={view} onChange={viewTabClicked} className={classes.tabs}>
+            {subNav.map((itemProps, index) => <Tab {...itemProps} key={`tab${index}:${itemProps.value}`} />)}
+          </Tabs>
+        </Grid>
       </Grid>
-    </Grid>
-    <Route path={routes.GENERAL} component={General} />
-    <Route path={routes.PROFILE} component={Profile} />
-  </Box>
-  )
+      <Route path={routes.GENERAL} component={General} />
+      <Route path={routes.PROFILE} component={Profile} />
+    </Box>
+  );
 };
 
 const mapStateToProps = (state, ownProps) => ({
   ...ownProps,
+  ...state.authReducer,
+  user: state.authReducer.data,
 });
 
 export default connect(mapStateToProps)(Dashboard);
--- src/pages/Dashboard/Profile/Profile.js
+++ src/pages/Dashboard/Profile/Profile.js
@@ -1,121 +1,51 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { makeStyles } from '@mui/styles';
-import {
-  Button, Grid, TextField, Typography,
-} from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import Loader from '@components/Loader/Loader';
-import { updateProfile } from '@redux/authuser/authuser.actions';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: `0 ${theme.spacing(32)}`,
   },
-  upload: {
-    width: '100%',
-    padding: `${theme.spacing(10)} ${theme.spacing(5)}`,
-    color: '#96C8ED',
-    borderColor: '#96C8ED',
-    borderStyle: 'dashed',
-  },
-  uploadText: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  finish: {
-    float: 'right',
+  displayData: {
+    paddingTop: theme.spacing(2.5),
+    borderBottom: `1px solid ${theme.palette.primary.contrastText}`,
+    '&:nth-child(even)': {
+      textAlign: 'end',
+    },
   },
 }));
 
-const Profile = ({ loading, user, dispatch }) => {
+const Profile = ({ loading, user }) => {
   const classes = useStyles();
-  const hiddenFileInput = useRef(null);
-  const [aboutMe, setAboutMe] = useState('');
-  const [file, setFile] = useState(null);
-
-  useEffect(() => {
-    if (user) {
-      setAboutMe(user.about_me);
-    }
-  }, [user]);
-
-  const handleClick = (event) => {
-    hiddenFileInput.current.click();
-  };
-
-  const handleFinish = (event) => {
-    const formData = new FormData();
-    if (file) {
-      formData.append('file', file[0], file[0].name);
-    }
-    formData.append('about_me', aboutMe);
-    formData.append('organization_name', user.organization.name);
-    dispatch(updateProfile(user.id, formData));
-    setFile(null);
-    setAboutMe('');
-  };
 
   return (
     <div className={classes.container}>
       {loading && <Loader open={loading} />}
       <Grid container>
-        <Grid item xs={3} mb={6}>
-          {user && !user.avatar_url && (
-            <Button variant="outlined" component="label" className={classes.upload} onClick={handleClick}>
-              {(file && file[0] && file[0].name) || 'Upload'}
-            </Button>
-          )}
-          {user && user.avatar_url && (
-            <>
-              <Button onClick={handleClick}>
-                <img src={user.avatar_url} alt={user.first_name} width="100%" />
-              </Button>
-              {file && file[0] && (
-                <Typography variant="caption" component="div">
-                  {`Change to: ${file[0].name}`}
-                </Typography>
-              )}
-            </>
-          )}
-          <input ref={hiddenFileInput} hidden accept="image/*" type="file" onChange={(e) => setFile(e.target.files)} />
-        </Grid>
-
-        <Grid item xs={8} ml={6} mb={6} className={classes.uploadText}>
-          <Typography variant="subtitle1" component="div">
-            Profile image
-          </Typography>
-          <Typography variant="caption" component="div">
-            Use JPEG or PNG formats, a self image or a logo.
-          </Typography>
-          <Typography variant="caption" component="div">
-            The image file shouldn’t be less than 4 mb.
-          </Typography>
-        </Grid>
-
-        <Grid item xs={12} mb={5}>
+        <Grid item xs={6} className={classes.displayData}>
           <Typography variant="body1" component="div">
-            About Me
+            Email
           </Typography>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            multiline
-            rows={10}
-            id="about_me"
-            name="about_me"
-            autoComplete="about_me"
-            placeholder="A short info about yourself or the company you represent."
-            value={aboutMe}
-            onChange={(e) => setAboutMe(e.target.value)}
-          />
         </Grid>
 
-        <Grid item xs={12}>
-          <Button variant="contained" onClick={handleFinish} className={classes.finish}>
-            Finish
-          </Button>
+        <Grid item xs={6} className={classes.displayData}>
+          <Typography variant="body1" component="div">
+            {user.email}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={6} className={classes.displayData}>
+          <Typography variant="body1" component="div">
+            Display name
+          </Typography>
+        </Grid>
+
+        <Grid item xs={6} className={classes.displayData}>
+          <Typography variant="body1" component="div">
+            {`${user.first_name} ${user.last_name}`}
+          </Typography>
         </Grid>
       </Grid>
     </div>
